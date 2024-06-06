053c6e3ea507050f200fc891b66319c67fd790b5
Godwill Kiplagat <godwillkiprop@gmail.com>
2024-06-06 12:29:43 +0300

    create a base folder using django-admin startproject base

--- studybud/urls.py
+++ studybud/urls.py
@@ -2,7 +2,13 @@
 from django.contrib import admin
 from django.urls import path
 
+
+
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('admin/', admin.site.urls),
+    path('', home),
+    path('room/', room),
+   
 ]
